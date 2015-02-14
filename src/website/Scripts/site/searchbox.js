514708cbea0f60657b0401c21b020f182dd8d21e
Matt Cosand <matt@cosand.net>
2015-02-14 14:33:21 -0800

    search box clears list when picked, some keyboard support

--- src/website/Scripts/site/searchbox.js
+++ src/website/Scripts/site/searchbox.js
@@ -9,9 +9,12 @@ ko.components.register('search-box', {
     this.results = ko.observableArray();
     this.ajax = null;
     this.debounce = null;
+    this.highlighted = ko.observable(-1);
     this.selected = (params && params.selected) ? params.selected : ko.observable();
 
+    var ignoreUpdate = false;
     var _searchHandler = function () {
+      if (ignoreUpdate) { return; }
       if (self.debounce) {
         window.clearTimeout(self.debounce);
         self.debounce = window.setTimeout(_processQuery, 300);
@@ -37,20 +40,50 @@ ko.components.register('search-box', {
 
     this.clickItem = function (item) {
       self.results.removeAll();
+      ignoreUpdate = true;
       self.text(item.text);
+      ignoreUpdate = false;
       self.selected(item);
     }
+
+    this.blur = function () {
+      window.setTimeout(self.results.removeAll, 5);
+    }
+
+    this.keyup = function (model, evt) {
+      var val = self.text();
+      var list = self.results();
+      var high = self.highlighted();
+      if (evt.keyCode == 40 && high < list.length - 1) { self.highlighted(high + 1) }
+      else if (evt.keyCode == 38 && high > 0) { self.highlighted(high - 1) }
+      else if (evt.keyCode == 13 && high >= 0) { self.clickItem(list[high]) }
+    }
+
+    this.doHighlight = function(model, event)
+    {
+      console.log('doing highlight');
+      var list = self.results();
+      console.log(list);
+      console.log(model);
+      for (var i = 0; i < list.length; i++)
+      {
+        if (model === list[i]) { self.highlighted(i); }
+      }
+    }
+
     this.doFocus = function () {
       $(window).trigger('resize');
+      var list = self.results();
+      if (list.length == 0) { _processQuery(); }
     }
   },
   template: '<form class="form-group has-feedback">' +
               '<label class="control-label sr-only">Search site</label>' +
-              '<input type="text" class="form-control" placeholder="Search" data-bind="textInput: text, event: { mouseover: doFocus, focus: doFocus}" />' +
+              '<input type="text" class="form-control" placeholder="Search" data-bind="textInput: text, event: { keyup: keyup, focus: doFocus, blur: blur}" />' +
               '<div class="search-box-dropdown glue-previous" data-bind="visible: hasResults, foreach: results">' +
-                '<div data-bind="click: $parent.clickItem" class="search-box-item">' +
-                  '<img data-bind="attr: { src: appRoot + \'/api/members/getthumbnail/\' + more.id + \'?width=25\' }" />' +
-                  '<div><strong data-bind="text: text"></strong><span data-bind="visible: more.dem, text: \' -  \' + more.dem"></span></div>' +
+                '<div data-bind="click: $parent.clickItem, event: {mouseover: $parent.doHighlight}, css: { \'search-box-highlight\': $parent.highlighted() == $index() }" class="search-box-item">' +
+                    '<img data-bind="attr: { src: appRoot + \'/api/members/getthumbnail/\' + more.id + \'?width=25\' }" />' +
+                    '<div><strong data-bind="text: text"></strong><span data-bind="visible: more.dem, text: \' -  \' + more.dem"></span></div>' +
                 '</div>' +
               '</div>' +
               '<span class="form-control-feedback fa" data-bind="css: { \'fa-search\': !working(), \'fa-circle-o-notch fa-spin\': working()}"></span>' +
