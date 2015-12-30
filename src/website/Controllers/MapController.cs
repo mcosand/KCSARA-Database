92a3410c335cf2fce87a640d6a77b99ae4b89239
Matt Cosand <matt@cosand.net>
2015-12-29 17:21:53 -0800

    Progress on new event modal

---
+++ src/website/Controllers/MapController.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright 2015 Matthew Cosand
+ */
+namespace Kcsara.Database.Web.Controllers
+{
+  using System;
+  using Kcsar.Database.Model;
+  using Kcsara.Database.Web.ViewModels;
+  using log4net;
+  using Microsoft.AspNet.Mvc;
+
+  public class MapController : BaseController
+  {
+    public MapController(Lazy<IKcsarContext> dbFactory, ILog log)
+      : base(dbFactory, log)
+    {
+    }
+
+    [HttpGet]
+    [Route("api/map/find/{type}")]
+    public SearchResult[] ApiFind(string type, string q, int limit = 10)
+    {
+      return new SearchResult[0];
+    }
+  }
+}
