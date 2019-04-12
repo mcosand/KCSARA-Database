f0b4d3b94a746a6db5365af72643d05c6fdf98ed
Matt Cosand <matt@cosand.net>
2019-04-12 16:10:25 -0700

    update OIDC config. Use longer time for cookies.

--- src/website/Startup.cs
+++ src/website/Startup.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.Configuration;
+using System.Globalization;
 using System.IdentityModel.Tokens;
 using System.Linq;
+using System.Security.Authentication;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityModel;
 using IdentityModel.Client;
 using Kcsara.Database.Api;
-using log4net;
 using Microsoft.IdentityModel.Protocols;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
@@ -34,102 +36,87 @@ namespace Kcsara.Database.Web
 
       app.Map("/api2", apiApp => apiApp.UseDatabaseApi(kernel));
 
+      NameValueCollection configStrings = ConfigurationManager.AppSettings;
+
+      app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
       app.UseCookieAuthentication(new CookieAuthenticationOptions
       {
-        AuthenticationType = "Cookies"
+        AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+        ExpireTimeSpan = TimeSpan.FromHours(1)
       });
 
-      NameValueCollection configStrings = ConfigurationManager.AppSettings;
+      JwtSecurityTokenHandler.InboundClaimTypeMap.Clear();
+
+      string identityUrl = configStrings["auth:authority"].Trim('/') + "/";
 
       app.Use<NonceCleanupOpenIdConnectAuthenticationMiddleware>(app, new OpenIdConnectAuthenticationOptions
       {
-        Authority = configStrings["auth:authority"].Trim('/') + "/",
-        ClientId = configStrings["auth:clientId"],
-        RedirectUri = configStrings["auth:redirect"].Trim('/') + "/",
-        ResponseType = "code id_token token",
-        Scope = "openid email profile",
+        AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+        SignInAsAuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+        RedirectUri = configStrings["auth:redirect"].Trim('/') + "/signin-oidc",
         PostLogoutRedirectUri = configStrings["auth:redirect"].Trim('/') + "/",
-        TokenValidationParameters = new TokenValidationParameters
-        {
-          NameClaimType = "name",
-          RoleClaimType = "role"
-        },
-        SignInAsAuthenticationType = "Cookies",
+        Authority = identityUrl,
+
+        ClientId = configStrings["auth:clientId"],
+        ClientSecret = configStrings["auth:secret"],
+
+        ResponseType = "code id_token",
+        Scope = "openid email profile",
+
         Notifications = new OpenIdConnectAuthenticationNotifications
         {
           AuthorizationCodeReceived = async n =>
           {
-            // use the code to get the access and refresh token
             var tokenClient = new TokenClient(
-            configStrings["auth:authority"].Trim('/') + "/connect/token",
-            configStrings["auth:clientId"],
-            configStrings["auth:secret"]);
+              identityUrl + "connect/token",
+              configStrings["auth:clientId"],
+              configStrings["auth:secret"]
+            );
 
-            var tokenResponse = await tokenClient.RequestAuthorizationCodeAsync(
-                n.Code, n.RedirectUri);
+            var tokenResponse = await tokenClient.RequestAuthorizationCodeAsync(n.Code, n.RedirectUri);
 
-            if (string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
+            if (tokenResponse.IsError)
             {
-              // For some reason the auth grant was invalid. Send the user to the destination page
-              // so they can re-generate the grant.
-              LogManager.GetLogger("Startup").ErrorFormat("No access token returned for grant {0}", n.Code);
-              n.Response.Redirect(n.RedirectUri);
-              n.HandleResponse();
-              return;
+              throw new AuthenticationException(tokenResponse.Error);
             }
 
-            // use the access token to retrieve claims from userinfo
-            var userInfoClient = new UserInfoClient(
-            new Uri(configStrings["auth:authority"].Trim('/') + "/connect/userinfo"),
-            tokenResponse.AccessToken);
+            var userInfoClient = new UserInfoClient(new Uri(identityUrl + "connect/userinfo"), tokenResponse.AccessToken);
 
             var userInfoResponse = await userInfoClient.GetAsync();
 
-            // create new identity
             var id = new ClaimsIdentity(n.AuthenticationTicket.Identity.AuthenticationType);
-            id.AddClaims(userInfoResponse.GetClaimsIdentity().Claims);
+            id.AddClaims(userInfoResponse.Claims.Select(f => new Claim(f.Item1, f.Item2)));
 
-            var accountId = new Guid(id.FindFirst("sub").Value);
-
+            id.AddClaim(new Claim("access_token", tokenResponse.AccessToken));
+            id.AddClaim(new Claim("expires_at", DateTime.Now.AddSeconds(tokenResponse.ExpiresIn).ToLocalTime().ToString(CultureInfo.InvariantCulture)));
+            id.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+            id.AddClaim(new Claim("sid", n.AuthenticationTicket.Identity.FindFirst("sid").Value));
             var rolesService = kernel.Get<IRolesService>();
+            var accountId = new Guid(id.FindFirst("sub").Value);
             foreach (var role in rolesService.ListAllRolesForAccount(accountId))
             {
               id.AddClaim(new Claim("role", role));
             }
 
-            id.AddClaim(new Claim("access_token", tokenResponse.AccessToken));
-            id.AddClaim(new Claim("expires_at", DateTime.Now.AddSeconds(tokenResponse.ExpiresIn).ToLocalTime().ToString()));
-            if (!string.IsNullOrWhiteSpace(tokenResponse.RefreshToken))
-            {
-              id.AddClaim(new Claim("refresh_token", tokenResponse.RefreshToken));
-            }
-            id.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
-
-            id.AddClaim(new Claim("sid", n.JwtSecurityToken.Claims.First(f => f.Type == "sid").Value));
-
+            n.AuthenticationTicket.Properties.ExpiresUtc = DateTimeOffset.UtcNow.AddHours(1);
             n.AuthenticationTicket = new AuthenticationTicket(
-                new ClaimsIdentity(id.Claims, n.AuthenticationTicket.Identity.AuthenticationType, "name", "role"),
-                n.AuthenticationTicket.Properties);
-
+              new ClaimsIdentity(id.Claims, n.AuthenticationTicket.Identity.AuthenticationType, JwtClaimTypes.Name, JwtClaimTypes.Role),
+              n.AuthenticationTicket.Properties
+            );
           },
 
-          AuthenticationFailed = n =>
-          {
-            LogManager.GetLogger(typeof(Startup)).Debug("Auth Failed", n.Exception);
-            return Task.FromResult(0);
-          },
           RedirectToIdentityProvider = n =>
           {
-            // if signing out, add the id_token_hint
-            if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+            if (n.ProtocolMessage.RequestType != OpenIdConnectRequestType.LogoutRequest)
             {
-              var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+              return Task.FromResult(0);
+            }
 
-              if (idTokenHint != null)
-              {
-                n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
-              }
+            var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
 
+            if (idTokenHint != null)
+            {
+              n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
             }
 
             return Task.FromResult(0);
